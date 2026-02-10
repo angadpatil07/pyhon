eecf3b56f20fe4943e561ea46536e9e240979aba
angad07 <angadpatil591@gmail.com>
2026-02-10 12:55:24 +0000

    added

---
+++ Practice Set 3.py
@@ -0,0 +1,11 @@
+i=int(input("Enter i "))
+if i==10:
+    print(i)
+elif i>10:
+    print("i is greater than 10")
+elif i>15:
+    print("i is greater than 15") 
+elif i>20:
+    print("i is greater than 20")
+elif i==25:
+    print("i is equal to 25")
